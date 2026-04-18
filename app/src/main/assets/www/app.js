e5ca6164376f6c87a7f1e8dbff9f222edbe47cb6
vraxyxx <veaxdev36@gmail.com>
2026-04-18 16:02:34 +0800

    Full QR Neon APK system

---
+++ app/src/main/assets/www/app.js
@@ -0,0 +1,11 @@
+function generate(){
+let text = document.getElementById("text").value;
+
+document.getElementById("qrcode").innerHTML = "";
+
+new QRCode(document.getElementById("qrcode"),{
+text:text,
+width:200,
+height:200
+});
+}
